209b7d5d3f837757db15a5a910a2a58962f80322
Alyx DARENNE <adarenne@greenweez.com>
2024-04-23 22:24:31 +0200

    feat: add auth-button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+import { AuthButton } from "../components/auth-button";
+import { ModeToggle } from "@/components/toggle-mode";
+import { cn } from "@/lib/utils";
+
 export default function Home() {
-  return <main>Home</main>;
+  return (
+    <main className={cn("flex justify-between")}>
+      <AuthButton>Sign in</AuthButton>
+      <ModeToggle />
+    </main>
+  );
 }
