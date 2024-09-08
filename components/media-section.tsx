91184cc60048306b9c6044073bf5a8e0fa719609
Alyx DARENNE <axda.web@gmail.com>
2024-09-08 16:41:48 +0200

    feat: replace horizontal scroll by carousel component

--- components/media-section.tsx
+++ components/media-section.tsx
@@ -1,6 +1,7 @@
 import { cn } from "@/lib/utils";
 
 import { MediaCard } from "./media-card";
+import { TrendingMediasCarousel } from "./trending-medias-carousel";
 import type { Media, Thumbnail } from "@/drizzle/schema";
 
 type MediaType = Media & { thumbnails: Thumbnail[] };
@@ -11,23 +12,33 @@ interface MediaCardProps {
 }
 
 export function MediaSection({ title, isTrending, medias }: MediaCardProps) {
-  const sectionStyling = isTrending
-    ? cn("flex gap-x-4 overflow-x-auto hide-scrollbar", "md:gap-x-10")
-    : cn(
-        "grid grid-cols-2 gap-4",
-        "md:grid-cols-3 md:gap-6",
-        "lg:grid-cols-4 lg:gap-10"
-      );
+  // const sectionStyling = isTrending
+  //   ? cn("flex gap-x-4 overflow-x-auto hide-scrollbar", "md:gap-x-10")
+  //   : cn(
+  //       "grid grid-cols-2 gap-4",
+  //       "md:grid-cols-3 md:gap-6",
+  //       "lg:grid-cols-4 lg:gap-10"
+  //     );
   return (
     <section className={cn("mb-6", "md:mb-10")}>
       <h2 className={cn("text-xl font-light mb-4", "md:text-3xl md:mb-6")}>
         {title}
       </h2>
-      <div className={sectionStyling}>
-        {medias.map((media) => (
-          <MediaCard key={media.id} isTrending={isTrending} media={media} />
-        ))}
-      </div>
+      {isTrending ? (
+        <TrendingMediasCarousel medias={medias} />
+      ) : (
+        <div
+          className={cn(
+            "grid grid-cols-2 gap-4",
+            "md:grid-cols-3 md:gap-6",
+            "lg:grid-cols-4 lg:gap-10"
+          )}
+        >
+          {medias.map((media) => (
+            <MediaCard key={media.id} isTrending={isTrending} media={media} />
+          ))}
+        </div>
+      )}
     </section>
   );
 }
